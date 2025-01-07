f121853f813ca5a4fb1c6cecfc54ae636d8e9468
carlpatton <carl.patton@posteo.net>
2025-01-07 11:41:18 +0100

    Update Reponse_11.py

--- Reponse_11.py
+++ Reponse_11.py
@@ -1,57 +1,62 @@
 import os
-from functools import cache
+import sys
+from collections import defaultdict
 
 def get_data():
     cwd = os.getcwd()
-    data = []
     with open(cwd + '/Jour_11_input.txt', 'r') as file:
-        line = file.readline().strip()
-    for number in line.split():
-        data.append(int(number))
-    return data
+        string_list = file.read().strip().split()
+    return [int(string) for string in string_list]
+
+def blink_part1(array):
+    res = []
+    for x in array:
+        if x == 0:
+            res.append(1)
+        elif len(str(x)) % 2 == 0:
+            l = len(str(x))
+            res += [int(str(x)[:l//2]), int(str(x)[l//2:])]
+        else:
+            res += [x * 2024]
+    return res
 
 def part1():
     stones = get_data()
     blinks = 25
     for i in range(blinks):
-        # print(i) # to see how fast it goes
-        new_stones = []
-        for stone in stones:
-            if stone == 0: # if stone is 0
-                new_stones.append(1)
-            elif len(str(stone)) % 2 == 0: # if even digits
-                half_len = len(str(stone)) // 2
-                left_half = int(str(stone)[:half_len])
-                right_half = int(str(stone)[half_len:])
-                new_stones.extend([left_half, right_half])
-            else: # otherwise, multiply stone by 2024
-                new_stones.append(stone * 2024) # order doesnt matter so just append them
-        stones = new_stones
-
+        stones = blink_part1(stones)
     return len(stones)
 
-@cache
-def process_stone(value, remaining_blinks):
-    if remaining_blinks == 0:  # Base case: no blinks left
-        return 1
-    if value == 0:  # if stone is 0
-        return process_stone(1, remaining_blinks - 1)
+sys.setrecursionlimit(2**30)
 
-    value_str = str(value)
-    length = len(value_str)
 
-    if length % 2 == 0:  # if even digits
-        midpoint = length // 2
-        left_half = int(value_str[:midpoint])
-        right_half = int(value_str[midpoint:])
-        return process_stone(left_half, remaining_blinks - 1) + process_stone(right_half, remaining_blinks - 1)
-    else:  # otherwise, multiply stone by 2024
-        new_value = value * 2024
-        return process_stone(new_value, remaining_blinks - 1)
+def data_to_dict(data):
+    entiers = defaultdict(int)
+    for x in data:
+        entiers[x] += 1
+    return entiers
+
+def blink_part2(entiers):
+    nouveaux_entiers = defaultdict(int)
+    for x in entiers:
+        l = len(str(x))
+        if x == 0:
+            nouveaux_entiers[1] += entiers[0]
+        elif l % 2 == 0:
+            nouveaux_entiers[int(str(x)[:l//2])] += entiers[x]
+            nouveaux_entiers[int(str(x)[l//2:])] += entiers[x]
+        else:
+            nouveaux_entiers[x * 2024] += entiers[x]
+
+    return nouveaux_entiers
 
 def part2():
-    stones = get_data()
-    total_stones = 0
-    for stone in stones: # for each stone we see how many they make
-        total_stones += process_stone(stone, 75)
-    return total_stones
+    entiers = data_to_dict(get_data())
+    blinks = 75
+    for i in range(blinks):
+        entiers = blink_part2(entiers)
+
+    resultat = 0
+    for x in entiers:
+        resultat += entiers[x]
+    return resultat
